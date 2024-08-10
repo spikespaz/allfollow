43d15b0c00fc1082c6bd0399215c03cdef0f3cdb
Jacob Birkett <jacob@birkett.dev>
2024-08-10 04:49:43 -0700

    main: imitate follows behavior

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod flake_lock;
 
 use flake_lock::{LockFile, MAX_SUPPORTED_LOCK_VERSION, MIN_SUPPORTED_LOCK_VERSION};
 
+use self::flake_lock::NodeEdge;
+
 fn main() {
     let file_content = std::fs::read_to_string("./samples/hyprnix/before/flake.lock")
         .expect("samples/a/flake.lock does not exist");
 
-    let mut lock: LockFile = {
+    let lock: LockFile = {
         let deser = &mut serde_json::Deserializer::from_str(&file_content);
         match serde_path_to_error::deserialize(deser) {
             Ok(lock) => lock,
@@ -23,5 +25,18 @@ fn main() {
         );
     }
 
-    dbg!(lock.follow_path(["hyprland", "aquamarine", "systems"]));
+    let root = lock.root();
+
+    for (_, edge) in root.iter_edges() {
+        let NodeEdge::Indexed(index) = &*edge else {
+            continue;
+        };
+        let input = &*lock.get_node(index).unwrap();
+        for (name, mut edge) in input.iter_edges_mut() {
+            let Some(root_edge) = root.get_edge(name) else {
+                continue;
+            };
+            *edge = (*root_edge).clone();
+        }
+    }
 }
