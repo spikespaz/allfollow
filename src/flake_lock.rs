62534219da51276293acab22d465fe2272d2d57a
Jacob Birkett <jacob@birkett.dev>
2024-08-14 05:06:39 -0700

    flake_lock: remove dead code

--- src/flake_lock.rs
+++ src/flake_lock.rs
@@ -39,66 +39,14 @@ pub struct LockedNode {
     flake: bool,
     #[serde(skip_serializing_if = "HashMap::is_empty", default)]
     inputs: HashMap<String, RefCell<NodeEdge>>,
-    locked: Box<LockedReference>,
-    original: Box<FlakeReference>,
+    locked: serde_json::Value,
+    original: serde_json::Value,
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Default)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct UnlockedNode {
     inputs: HashMap<String, RefCell<NodeEdge>>,
-}
-
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct LockedReference {
-    last_modified: usize,
-    nar_hash: String,
-    #[serde(flatten)] // do not use deny_unknown_fields
-    flake_ref: FlakeReference,
-}
-// Supposedly using `deny_unknown_fields` here is illegal when the outer
-// `LockedReference` flattens into `flake_ref`.
-// I have tested it a bit, and it seems to work, and catch errors.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-#[serde(
-    deny_unknown_fields,
-    tag = "type",
-    rename_all = "camelCase",
-    rename_all_fields = "camelCase"
-)]
-pub enum FlakeReference {
-    Indirect {
-        id: String,
-        #[serde(skip_serializing_if = "Option::is_none", default)]
-        rev: Option<String>,
-    },
-    Tarball {
-        url: String,
-    },
-    Git {
-        url: String,
-        #[serde(skip_serializing_if = "Option::is_none", default)]
-        r#ref: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none", default)]
-        rev: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none", default)]
-        rev_count: Option<u32>,
-        #[serde(skip_serializing_if = "std::ops::Not::not", default)]
-        submodules: bool,
-    },
-    Github {
-        owner: String,
-        repo: String,
-        #[serde(skip_serializing_if = "Option::is_none", default)]
-        r#ref: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none", default)]
-        rev: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none", default)]
-        dir: Option<String>,
-        #[serde(skip_serializing_if = "std::ops::Not::not", default)]
-        submodules: bool,
-    },
 }
 
 impl NodeEdge {
@@ -189,15 +137,6 @@ impl Node {
             .get(name.as_ref())
             .map(|cell| cell.borrow_mut())
     }
-
-    // pub fn iter_inputs<'lock>(
-    //     &'lock self,
-    //     lock: &'lock LockFile,
-    // ) -> impl Iterator<Item = (&str, Option<(&str, Ref<Node>)>)> {
-    //     self.edges()
-    //         .iter()
-    //         .map(|(name, edge)| (name.as_str(), lock.resolve_edge(edge)))
-    // }
 }
 
 impl LockFile {
@@ -257,101 +196,4 @@ impl LockFile {
             self.resolve_edge(&*self.get_node(index)?.get_edge(name)?)
         })
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::flake_lock::LockedNode;
-
-    use super::Node;
-
-    #[test]
-    fn parse_node_aquamarine() {
-        let aquamarine_json = serde_json::json!(
-            {
-                "inputs": {
-                    "hyprutils": [
-                        "hyprutils"
-                    ],
-                    "hyprwayland-scanner": [
-                        "hyprwayland-scanner"
-                    ],
-                    "nixpkgs": [
-                        "nixpkgs"
-                    ],
-                    "systems": [
-                        "systems"
-                    ]
-                },
-                "locked": {
-                    "lastModified": 1722974790,
-                    "narHash": "sha256-zBZ9BvKF8pMF0ywcn1BI0+ntM1R0N8nysUmMDGmi0ts=",
-                    "owner": "hyprwm",
-                    "repo": "aquamarine",
-                    "rev": "131ed05f99e66c3e518be3fadeff7ddb7d1d351d",
-                    "type": "github"
-                },
-                "original": {
-                    "owner": "hyprwm",
-                    "repo": "aquamarine",
-                    "type": "github"
-                }
-            }
-        );
-        dbg!(serde_json::from_value::<Node>(aquamarine_json).unwrap());
-    }
-
-    #[test]
-    fn parse_node_hyprland() {
-        let hyprland_json = serde_json::json!(
-            {
-                "inputs": {
-                    "aquamarine": "aquamarine_2",
-                    "hyprcursor": "hyprcursor_2",
-                    "hyprlang": "hyprlang_2",
-                    "hyprutils": "hyprutils_3",
-                    "hyprwayland-scanner": "hyprwayland-scanner_2",
-                    "nixpkgs": "nixpkgs_4",
-                    "systems": "systems_4",
-                    "xdph": "xdph"
-                },
-                "locked": {
-                    "lastModified": 1723579231,
-                    "narHash": "sha256-PL9C3aOetj+TS+vXvNhh7q5bm3g70oakg+iSu5eQBUQ=",
-                    "ref": "refs/heads/main",
-                    "rev": "3b4aabe04c7756fb0a70d78b6f0e701228f46345",
-                    "revCount": 5087,
-                    "submodules": true,
-                    "type": "git",
-                    "url": "https://github.com/hyprwm/Hyprland"
-                },
-                "original": {
-                    "submodules": true,
-                    "type": "git",
-                    "url": "https://github.com/hyprwm/Hyprland"
-                }
-            }
-        );
-        dbg!(serde_json::from_value::<LockedNode>(hyprland_json).unwrap());
-    }
-
-    //     macro_rules! parse_lock_file {
-    //         ($test_ident:ident, $lock_file_path:literal) => {
-    //             #[test]
-    //             fn $test_ident() {
-    //                 static SOURCE: &str = include_str!($lock_file_path);
-    //                 let deser = &mut serde_json::Deserializer::from_str(SOURCE);
-    //                 let res: Result<FlakeLock, _> = serde_path_to_error::deserialize(deser);
-    //                 match res {
-    //                     Ok(lock) => {
-    //                         dbg!(lock);
-    //                     }
-    //                     Err(e) => panic!("{}", e),
-    //                 }
-    //             }
-    //         };
-    //     }
-
-    //     parse_lock_file!(parse_hyprnix_before, "../samples/hyprnix/before/flake.lock");
-    //     parse_lock_file!(parse_hyprnix_after, "../samples/hyprnix/after/flake.lock");
 }
