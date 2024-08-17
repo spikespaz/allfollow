a3498488009249a2f4d296dbfad79b46a0e1dbc2
Jacob Birkett <jacob@birkett.dev>
2024-08-16 22:01:26 -0700

    fmt_colors: improve macro hygiene

--- src/fmt_colors.rs
+++ src/fmt_colors.rs
@@ -1,7 +1,9 @@
+//! If you borrow this, please give me credit.
+
 #[macro_export]
 macro_rules! elogln {
-    ($($args:tt)*) => {
-        eprintln!("{}", format_args_colored!($($args)*))
+    ( $($args:tt)* ) => {
+        eprintln!("{}", $crate::format_args_colored!( $($args)* ))
     };
 }
 
@@ -9,48 +11,48 @@ macro_rules! elogln {
 #[macro_export]
 macro_rules! format_args_colored {
     // By default, all literals are treated as format strings.
-    ( $(: $style:ident)* $literal:literal $($tail:tt)* ) => {
-        format_args!( "{}{}", format_args!( $literal ) $(.$style())* , format_args_colored!( $($tail)* ) )
+    ( $(: $style:ident)* $format:literal $($tail:tt)* ) => {
+        format_args!( "{}{}", format_args!( $format ) $(.$style())* , $crate::format_args_colored!( $($tail)* ) )
     };
 
     // To display a literal value, escape it with a reference/ampersand.
     ( $(: $style:ident)* & $literal:literal $($tail:tt)* ) => {
-        format_args!( "{}{}", $literal $(.$style())* , format_args_colored!( $($tail)* ) )
+        format_args!( "{}{}", $literal $(.$style())* , $crate::format_args_colored!( $($tail)* ) )
     };
 
     // Bare identifiers are not allowed, but references are.
     ( $(: $style:ident)* & $ident:ident $($tail:tt)* ) => {
-        format_args!( "{}{}", $ident $(.$style())* , format_args_colored!( $($tail)* ) )
+        format_args!( "{}{}", $ident $(.$style())* , $crate::format_args_colored!( $($tail)* ) )
     };
 
     // Expressions must be placed in parentheses.
     ( $(: $style:ident)* ( $expr:expr ) $($tail:tt)* ) => {
-        format_args!( "{}{}", ( $expr ) $(.$style())* , format_args_colored!( $($tail)* ) )
+        format_args!( "{}{}", ( $expr ) $(.$style())* , $crate::format_args_colored!( $($tail)* ) )
     };
 
     // Inline expression blocks are allowed.
     ( $(: $style:ident)* { $($expr:tt)+ } $($tail:tt)* ) => {
-        format_args!( "{}{}", { $($expr)+ } $(.$style())* , format_args_colored!( $($tail)* ) )
+        format_args!( "{}{}", { $($expr)+ } $(.$style())* , $crate::format_args_colored!( $($tail)* ) )
     };
 
     // Bare parentheses with multiple items will recurse colored formatting.
     ( $(: $style:ident)* ( $($recurse:tt)+ ) $($tail:tt)* ) => {
-        format_args!( "{}{}", format_args_colored!( $($recurse)+ ) $(.$style())* , format_args_colored!( $($tail)* ) )
+        format_args!( "{}{}", format_args_colored!( $($recurse)+ ) $(.$style())* , $crate::format_args_colored!( $($tail)* ) )
     };
 
     // Parentheses prefixed with a period are treated as normal format arguments.
     ( $(: $style:ident)* . ( $($format_args:tt)+ ) $($tail:tt)* ) => {
-        format_args!( "{}{}", format_args!( $($format_args)+ ) $(.$style())* , format_args_colored!( $($tail)* ) )
+        format_args!( "{}{}", format_args!( $($format_args)+ ) $(.$style())* , $crate::format_args_colored!( $($tail)* ) )
     };
 
     // Comma has not been matched by previous recursion, prepend a space.
     ( , $($tail:tt)* ) => {
-        format_args!( " {}", format_args_colored!( $($tail)* ))
+        format_args!( " {}", $crate::format_args_colored!( $($tail)* ))
     };
 
     // Semicolon has not been matched by previous recursion, prepend a newline.
     ( ; $($tail:tt)* ) => {
-        format_args!( "\n{}", format_args_colored!( $($tail)* ))
+        format_args!( "\n{}", $crate::format_args_colored!( $($tail)* ))
     };
 
     // Terminated.
@@ -92,8 +94,7 @@ mod tests {
         );
         elogln!(:yellow "formatted {large_number}", :red (usize::MAX));
         let no_copy = NoCopy(vec!["no".to_owned(), "copy".to_owned()]);
-        elogln!(:yellow (&no_copy), :red (usize::MAX));
-        drop(no_copy);
+        elogln!(:yellow (no_copy), :red (usize::MAX));
         elogln!(:on_bright_white (
             :black "black"
             :yellow "yellow"
@@ -104,5 +105,6 @@ mod tests {
             let b = 5;
             a + b
         });
+        elogln!(:green .("This is {} {} {}", "normal", "formatted", "text"))
     }
 }
