eb3c905f057d45bb9076283463d70e2fecf8c590
Jacob Birkett <jacob@birkett.dev>
2024-08-28 07:00:48 -0700

    flake: devShell: remove openssl

--- flake.nix
+++ flake.nix
@@ -45,28 +45,20 @@
             extensions = [ "rust-src" "rust-docs" "clippy" ];
           };
         in {
-          default = with pkgs; # remove `with` at your option
-            mkShell {
-              strictDeps = true;
-              packages = [
-                # Derivations in `rust-stable` take precedence over nightly.
-                (lib.hiPrio rust-stable)
+          default = pkgs.mkShell {
+            strictDeps = true;
+            packages = with pkgs; [
+              # Derivations in `rust-stable` take precedence over nightly.
+              (lib.hiPrio rust-stable)
 
-                # Use rustfmt, and other tools that require nightly features.
-                (pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-                  toolchain.minimal.override {
-                    extensions = [ "rustfmt" "rust-analyzer" ];
-                  }))
-
-                # Native transitive dependencies for Cargo
-                pkg-config
-                openssl
-              ];
-              OPENSSL_LIB_DIR = "${openssl.out}/lib";
-              OPENSSL_ROOT_DIR = "${openssl.out}";
-              OPENSSL_INCLUDE_DIR = "${openssl.dev}/include";
-              # RUST_BACKTRACE = 1;
-            };
+              # Use rustfmt, and other tools that require nightly features.
+              (rust-bin.selectLatestNightlyWith (toolchain:
+                toolchain.minimal.override {
+                  extensions = [ "rustfmt" "rust-analyzer" ];
+                }))
+            ];
+            # RUST_BACKTRACE = 1;
+          };
         });
 
       formatter = eachSystem (system: pkgsFor.${system}.nixfmt-classic);
