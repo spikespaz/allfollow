bf09cf5e6e3f5b2530ca5754cf3e0a515d611f38
Jacob Birkett <jacob@birkett.dev>
2025-07-08 02:46:33 -0700

    nix: formatter: handle too many arguments

--- nix/formatter.nix
+++ nix/formatter.nix
@@ -7,6 +7,16 @@ writeShellApplication {
       (toolchain: toolchain.minimal.override { extensions = [ "rustfmt" ]; }))
   ];
   text = ''
+    bad_usage() {
+      echo -e "\033[31merror:\033[0m $1" >&2
+      echo -e "\033[33mUsage:\033[0m $(basename "$0") [ . | -- ]" >&2
+      exit 1
+    }
+
+    if [ "$#" -gt 1 ]; then
+      bad_usage "too many arguments"
+    fi
+
     src="''${1:-.}"
     case "$src" in
       --)
@@ -17,8 +27,7 @@ writeShellApplication {
         cargo fmt
         ;;
       *)
-        echo "Usage: $(basename "$0") [ . | -- ]" >&2
-        exit 1
+        bad_usage "unknown argument '$src'"
         ;;
     esac
   '';
