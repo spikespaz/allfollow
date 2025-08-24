66552a4de3d8454ec43c23dbf23fb05b94d8c34e
Jacob Birkett <jacob@birkett.dev>
2025-08-24 04:32:26 -0700

    nix-api: fetchers: impl PublicKey

--- nix-api/src/fetchers/mod.rs
+++ nix-api/src/fetchers/mod.rs
@@ -1,1 +1,10 @@
+use serde::{Deserialize, Serialize};
+
 pub mod git;
+
+// <https://github.com/NixOS/nix/blob/c9211b0b2d52a26ed666780b763b39a5bddd3fb3/src/libfetchers/include/nix/fetchers/fetchers.hh#L274>
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct PublicKey {
+    pub r#type: String,
+    pub key: String,
+}
