13959eb19f399bc0426727cfc053a5180af83169
Jacob Birkett <jacob@birkett.dev>
2025-08-27 18:14:14 -0700

    nix-api: tests: hashes: impl concurrent store queries

--- nix-api/tests/nixpkgs-hashes/build.rs
+++ nix-api/tests/nixpkgs-hashes/build.rs
@@ -2,17 +2,19 @@ use std::collections::HashSet;
 use std::ffi::{OsStr, OsString};
 use std::os::unix::process::ExitStatusExt;
 use std::process::{ExitStatus, Stdio};
-use std::sync::LazyLock;
+use std::sync::{Arc, LazyLock};
 use std::time::{Duration, Instant};
 
 use smol::future::try_zip;
 use smol::io::{AsyncBufReadExt, BufReader};
+use smol::lock::Semaphore;
 use smol::process::Command;
 use smol::stream::{Stream, StreamExt, try_unfold};
 use smol::{LocalExecutor, channel};
 use sonic_rs::{JsonValueTrait, LazyValue, PointerTree};
 
 const STORE_PATHS_PER_QUERY: usize = 8;
+const MAX_CONCURRENT_STORE_QUERIES: usize = 4;
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 struct Hash {
@@ -46,8 +48,10 @@ fn main() -> std::io::Result<()> {
     }
 
     println!("STORE_PATHS_PER_QUERY = {STORE_PATHS_PER_QUERY}");
+    println!("MAX_CONCURRENT_STORE_QUERIES = {MAX_CONCURRENT_STORE_QUERIES}");
 
     let ex = LocalExecutor::new();
+    let sem = Arc::new(Semaphore::new(MAX_CONCURRENT_STORE_QUERIES));
     let (chunks_tx, chunks_rx) = channel::unbounded();
     let (stats_tx, stats_rx) = channel::bounded(1);
 
@@ -70,8 +74,14 @@ fn main() -> std::io::Result<()> {
             if batch.is_empty() {
                 break;
             }
-            let hashes = collect_hashes_for_many_derivations(batch).await;
-            chunks_tx.send(hashes).await.unwrap();
+            let permit = sem.acquire_arc().await;
+            let tx = chunks_tx.clone();
+            ex.spawn(async move {
+                let hashes = collect_hashes_for_many_derivations(batch).await;
+                tx.send(hashes).await.unwrap();
+                drop(permit);
+            })
+            .detach();
         }
 
         drop(chunks_tx);
